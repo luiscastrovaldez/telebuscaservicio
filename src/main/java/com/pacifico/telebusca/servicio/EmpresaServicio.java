1c0c126b1215d784f526fc0145ed14c339abb677
gluiscastro <gluiscastro@d3b968d0-30ef-8ba3-7492-d8022e9182e1>
2013-09-12 21:41:16 +0000

    agregando implementacion configuracion y test

--- src/main/java/com/pacifico/telebusca/servicio/EmpresaServicio.java
+++ src/main/java/com/pacifico/telebusca/servicio/EmpresaServicio.java
@@ -1,8 +1,12 @@
 package com.pacifico.telebusca.servicio;
 
+import java.io.Serializable;
+
 import com.pacifico.telebusca.dominio.Empresa;
 
 public interface EmpresaServicio {
 
 	void guardarEmpresa(Empresa empresa);
+	Empresa actualizarEmpresa(Empresa empresa);
+	Empresa buscarEmpresa(final Serializable pkEmpresa);
 }
--- src/test/java/com/pacifico/telebusca/servicio/EmpresaServicioTest.java
+++ src/test/java/com/pacifico/telebusca/servicio/EmpresaServicioTest.java
@@ -30,9 +30,50 @@ public class EmpresaServicioTest extends TeleBuscaTest {
 			empresa.setUsuarioCreacion("lcastro");
 			empresa.setUsuarioModificacion("lcastro");
 			this.empresaServicio.guardarEmpresa(empresa);
+			Assert.assertTrue(Boolean.TRUE);
 		} catch (Exception e) {
 			Assert.fail(e.getMessage());
 		}
 		System.out.println("****** Test Guardar Empresa ******");
 	}
+	
+	@Test
+	public void actualizarEmpresaTest() {
+		System.out.println("****** Test Actualizar Empresa ******");
+		Empresa empresa = new Empresa();
+		try {
+			empresa.setEstado('N');
+			empresa.setCarpetaAudio("/temp");
+			empresa.setEmpresa("Atento");
+			empresa.setFechaCreacion(new Timestamp(Calendar.getInstance()
+					.getTimeInMillis()));
+			empresa.setFechaModificacion(new Timestamp(Calendar.getInstance()
+					.getTimeInMillis()));
+			empresa.setUsuarioCreacion("lcastro");
+			empresa.setUsuarioModificacion("lcastro");
+			this.empresaServicio.guardarEmpresa(empresa);
+			Assert.assertTrue(Boolean.TRUE);
+		} catch (Exception e) {
+			Assert.fail(e.getMessage());
+		}
+		System.out.println("****** Test Actualizar Empresa ******");
+	}
+	@Test
+	public void buscarEmpresaTest() {
+		System.out.println("****** Test Buscar Empresa ******");
+		Empresa empresa = new Empresa();
+		try {			
+			empresa = this.empresaServicio.buscarEmpresa(10);
+			if (empresa == null) {
+				Assert.fail("No existe Empresa Registrada");
+			} else {
+				System.out.println("nombre de empresa = " + empresa.getEmpresa());
+				Assert.assertNotNull(empresa);
+			}
+		} catch (Exception e) {
+			Assert.fail(e.getMessage());
+		}
+		System.out.println("****** Test Actualizar Empresa ******");
+	}
+
 }
