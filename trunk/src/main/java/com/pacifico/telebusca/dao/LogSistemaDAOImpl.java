b6070ff901fa21c73bea54810311ee4627adfe09
gluiscastro <gluiscastro@d3b968d0-30ef-8ba3-7492-d8022e9182e1>
2013-09-14 19:05:34 +0000

    agregando implemtacion

--- trunk/src/main/java/com/pacifico/telebusca/dao/LogSistemaDAOImpl.java
+++ trunk/src/main/java/com/pacifico/telebusca/dao/LogSistemaDAOImpl.java
@@ -1,8 +1,10 @@
 package com.pacifico.telebusca.dao;
 
+import com.pacifico.telebusca.dominio.LogSistema;
+
 public class LogSistemaDAOImpl implements LogSistemaDAO {
 
-	public void guardaLogSistema(LogSistemaDAO logsistema) {
+	public void guardaLogSistema(LogSistema logsistema) {
 		// TODO Auto-generated method stub
 
 	}
