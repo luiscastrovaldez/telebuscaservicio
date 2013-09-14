efa006e6892fa6cda0b3c05f7ce344b3b6e2a3bf
gluiscastro <gluiscastro@d3b968d0-30ef-8ba3-7492-d8022e9182e1>
2013-09-14 19:05:34 +0000

    agregando implemtacion

--- src/main/java/com/pacifico/telebusca/dao/LogSistemaDAO.java
+++ src/main/java/com/pacifico/telebusca/dao/LogSistemaDAO.java
@@ -1,6 +1,8 @@
 package com.pacifico.telebusca.dao;
 
+import com.pacifico.telebusca.dominio.LogSistema;
+
 public interface LogSistemaDAO {
 
-	void guardaLogSistema(LogSistemaDAO logsistema);
+	void guardaLogSistema(LogSistema logsistema);
 }
