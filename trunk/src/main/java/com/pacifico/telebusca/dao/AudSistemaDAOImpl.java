9a21774fb6db1f579710c5f00f837cb465b81e27
gluiscastro <gluiscastro@d3b968d0-30ef-8ba3-7492-d8022e9182e1>
2013-10-08 16:00:37 +0000

    adding impl

--- trunk/src/main/java/com/pacifico/telebusca/dao/AudSistemaDAOImpl.java
+++ trunk/src/main/java/com/pacifico/telebusca/dao/AudSistemaDAOImpl.java
@@ -3,10 +3,13 @@ package com.pacifico.telebusca.dao;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.apache.log4j.Logger;
+
 import com.pacifico.telebusca.dominio.AudSistema;
 
 public class AudSistemaDAOImpl implements AudSistemaDAO {
 
+	
 	@PersistenceContext(name="teleBuscaPersistence")
 	private EntityManager em;
 	
