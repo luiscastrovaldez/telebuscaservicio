2984d6d3cb236ea8c55390c932e94e41222a1642
gluiscastro <gluiscastro@d3b968d0-30ef-8ba3-7492-d8022e9182e1>
2013-10-19 01:10:03 +0000

    add

--- src/main/java/com/pacifico/telebusca/dao/AudSistemaDAOImpl.java
+++ src/main/java/com/pacifico/telebusca/dao/AudSistemaDAOImpl.java
@@ -3,22 +3,19 @@ package com.pacifico.telebusca.dao;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import org.apache.log4j.Logger;
 import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
 
 import com.pacifico.telebusca.dominio.AudSistema;
 
-public class AudSistemaDAOImpl extends HibernateJpaDialect implements AudSistemaDAO {
+public class AudSistemaDAOImpl extends HibernateJpaDialect implements
+		AudSistemaDAO {
+
+	private static final long serialVersionUID = 1L;
+	@PersistenceContext(name = "teleBuscaPersistence")
+	private EntityManager em;
 
 	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	@PersistenceContext(name="teleBuscaPersistence")
-	private EntityManager em;
-	
-	public void guardaAudSistema(AudSistema audSistema) {		
+	public void guardaAudSistema(AudSistema audSistema) {
 		em.persist(audSistema);
 		em.flush();
 	}
