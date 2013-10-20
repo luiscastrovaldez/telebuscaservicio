ca205bce0f303ae8961d5b30d228fade12b191f8
gluiscastro <gluiscastro@d3b968d0-30ef-8ba3-7492-d8022e9182e1>
2013-10-20 03:52:58 +0000

    add

--- src/main/java/com/pacifico/telebusca/servicio/xml/TeleBuscaAudio.java
+++ src/main/java/com/pacifico/telebusca/servicio/xml/TeleBuscaAudio.java
@@ -1,17 +1,17 @@
 package com.pacifico.telebusca.servicio.xml;
 
 import java.io.File;
-import java.util.List;
 
 import com.pacifico.telebusca.web.beans.ValidacionErrores;
 
 public interface TeleBuscaAudio {
 
-	ValidacionErrores parseaFileXml(File xml);
-	List<Object> parseaFileXml(String xml);
-	void escucharAudio();
-	void descargarAudio();
-	ValidacionErrores validarArchivoXml(File xml);
-	void descargarArchivoResgistroNoValidos();
-	ValidacionErrores validarArchivoXml(String xml);
+	void escucharAudio() throws Exception;
+
+	File descargarAudio() throws Exception;
+
+	ValidacionErrores validarArchivoXml(File xml) throws Exception;
+
+	File descargarArchivoResgistroNoValidos() throws Exception;
+
 }
