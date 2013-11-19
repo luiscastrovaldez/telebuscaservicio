6a52455a055d76947a4725e0f11836bb9e2b26e2
gluiscastro <gluiscastro@d3b968d0-30ef-8ba3-7492-d8022e9182e1>
2013-11-19 02:31:45 +0000

    Initial import.

--- trunk/src/main/java/com/pacifico/telebusca/dominio/AudSistema.java
+++ trunk/src/main/java/com/pacifico/telebusca/dominio/AudSistema.java
@@ -26,7 +26,7 @@ public class AudSistema implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	@Column(name = "CODAUDSIST")
 	private int codAudoSist;
 
--- trunk/src/main/java/com/pacifico/telebusca/dominio/Audio.java
+++ trunk/src/main/java/com/pacifico/telebusca/dominio/Audio.java
@@ -26,7 +26,7 @@ public class Audio implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	@Column(name = "CODAUDIO")
 	private int codAudio;
 
